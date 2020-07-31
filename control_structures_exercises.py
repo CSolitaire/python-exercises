040d4d0ae571d40b15d27d9e351d4b3da255286a
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-31 10:18:07 -0500

    Question 2 Complete

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -14,9 +14,8 @@ print("Thank You!, Have a nice day!")
 
 # Prompt the user for a day of the week, print out whether the day is a weekday or a weekend
 response = input("What day of the week is today?")
-if response.lower().strip() == "monday":
+if response.lower().strip() in weekday:
     weekday = ("monday","tuesday","wednesday","thursday","friday")
-    weekend = ("saturday", "sunday")
     print ("Boo! It is a weekday...")
 else:
     print ("Yee Haw! It's the weekend, party time!")
