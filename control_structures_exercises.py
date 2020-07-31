f84a357072446eec08033ef6f89398ad24aba8d6
Corey Solitaire <corey.solitaire@gmail.com>
2020-07-31 08:42:54 -0500

    Question 1 Complete

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -1,11 +1,22 @@
-print ("Hello")
-print ("More Hello")
+
 
 # 1.Conditional Basics
 
 #     prompt the user for a day of the week, print out whether the day is Monday or not
 
+response = input("What day of the week is today?")
+if response.lower().strip() == "monday":
+    print ("It is Monday")
+else:
+    print("Too bad it's not Monday...")
+print("Thank You!, Have a nice day!")
+   
+
 #     prompt the user for a day of the week, print out whether the day is a weekday or a weekend
+
+
+
+
 
 #     create variables and make up values for
 #         the number of hours worked in one week
