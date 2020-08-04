f60a1b8a3cf1817e315359b234d7422b11a38ebb
Corey Solitaire <corey.solitaire@gmail.com>
2020-08-04 11:15:58 -0500

    Question 1 Complete

--- function_exercises.py
+++ function_exercises.py
@@ -1,6 +1,21 @@
 
     # 1. Define a function named is_two. It should accept one input and return True if the passed input is either 
     #    the number or the string 2, False otherwise.
+def is_two(x):
+    if x in (2, 2.0, "two", "TWO", "Two"):
+        return True
+    else:
+        return False
+
+is_two("cheese")
+
+is_two(2)
+
+is_two(2.0)
+
+is_two(2.1)
+
+is_two("two")
 
 
 
